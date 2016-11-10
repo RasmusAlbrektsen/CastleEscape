eff357e4de5c728a712c316d16676425c75ce132
Sebastian <Sebastian@10.125.5.79>
2016-11-10 15:23:11 +0100

    Have made some a UI with Scene Builder

--- src/castleescape/gui/FXMLDocumentController.java
+++ src/castleescape/gui/FXMLDocumentController.java
@@ -10,7 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.web.WebView;
 
 /**
  *
@@ -18,15 +20,113 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
 	
+	// DropDown Menus
 	@FXML
-	private Label label;
+	private MenuButton roomButton;
+
+	@FXML
+	private MenuButton inventoryButton;
 	
 	@FXML
-	private void handleButtonAction(ActionEvent event) {
-		System.out.println("You clicked me!");
-		label.setText("Hello World!");
+	private MenuButton peakButton;
+	
+	//Buttons
+	@FXML
+	private Button useButton;
+
+	@FXML
+	private Button hintButton;	
+	
+	@FXML
+	private Button dropButton;
+		
+	@FXML
+	private Button takeButton;
+			
+	@FXML
+	private Button inspectButton;
+				
+	@FXML
+	private Button northButton;
+		
+	@FXML
+	private Button westButton;
+	
+	@FXML
+	private Button eastButton;
+	
+	@FXML
+	private Button southButton;
+	
+	//WebView
+	@FXML
+	private WebView webViewField;
+	
+	// DropDown Menus
+	@FXML
+	private void roomAction(ActionEvent event){
+		
 	}
 	
+	@FXML
+	private void inventoryAction(ActionEvent event){
+		
+	}
+	
+	@FXML
+	private void peakAction(ActionEvent event){
+		
+	}
+	
+	//Buttons
+	
+	@FXML
+	private void useAction(ActionEvent event){
+		
+	}
+	
+	@FXML
+	private void hintAction(ActionEvent event){
+		
+	}
+	
+	@FXML
+	private void dropAction(ActionEvent event){
+		
+	}
+	
+	@FXML
+	private void takeAction(ActionEvent event){
+			webViewField.getEngine().load("http://www.google.dk/");
+	}
+	
+	@FXML
+	private void inspectAction(ActionEvent event){
+		
+	}
+	
+	@FXML
+	private void northAction(ActionEvent event){
+		
+	}
+	
+	@FXML
+	private void westAction(ActionEvent event){
+		
+	}
+	
+	@FXML
+	private void eastAction(ActionEvent event){
+		
+	}
+	
+	
+	@FXML
+	private void southAction(ActionEvent event){
+		
+	}
+	
+		
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		// TODO
