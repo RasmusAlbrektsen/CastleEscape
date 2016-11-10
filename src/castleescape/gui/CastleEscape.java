b88d1a7b271b775b7031cc34f76fb91253afe425
Kasper <Kasper@LAPTOP-CM4GL9IK>
2016-11-10 21:25:23 +0100

    More work on GUI The gui can now display output from the game, but it needs some polishing.

--- src/castleescape/gui/CastleEscape.java
+++ src/castleescape/gui/CastleEscape.java
@@ -5,24 +5,56 @@
  */
 package castleescape.gui;
 
+import castleescape.business.BusinessMediator;
+import castleescape.business.framework.Game;
+import java.io.File;
+import java.util.Scanner;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import util.XMLFileLocater;
 
 /**
  *
  * @author Kasper
  */
 public class CastleEscape extends Application {
-	
+
+	private BusinessMediator initGame() {
+		//Folder containing the xml files
+		File xmlFolder = new File("xml");
+		//List of files in the xml folder
+		File[] fileList = xmlFolder.listFiles();
+		//Prompt for a selection between games, list the names of them
+		System.out.println("Which game would you like to play?:");
+		for (int i = 0; i < fileList.length; i++) {
+			System.out.println(i + 1 + ": " + fileList[i].getName());
+		}
+
+		Scanner input = new Scanner(System.in);
+		//Parse the selected folder with the XMLFileLocater
+		XMLFileLocater locater = new XMLFileLocater(fileList[input.nextInt() - 1].toPath());
+
+		//construct game instance
+		Game game = new Game();
+
+		return new BusinessMediator(game);
+	}
+
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-		
+		BusinessMediator bm = initGame();
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLDocument.fxml"));
+
+		Parent root = (Parent) loader.load();
+		FXMLDocumentController controller = loader.getController();
+		controller.setBusinessMediator(bm);
+
 		Scene scene = new Scene(root);
-		
+
 		stage.setScene(scene);
 		stage.show();
 	}
@@ -33,5 +65,5 @@ public class CastleEscape extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 }
--- src/castleescape/gui/FXMLDocumentController.java
+++ src/castleescape/gui/FXMLDocumentController.java
@@ -5,31 +5,145 @@
  */
 package castleescape.gui;
 
+import castleescape.business.BusinessMediator;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.web.WebView;
 
 /**
+ * The controller for the main game GUI.
  *
  * @author Kasper
  */
 public class FXMLDocumentController implements Initializable {
-	
+
+	/**
+	 * The mediator to use for making calls to business code.
+	 */
+	private BusinessMediator businessMediator;
+
+	//DropDown Menus
 	@FXML
-	private Label label;
-	
+	private MenuButton roomButton;
+
 	@FXML
-	private void handleButtonAction(ActionEvent event) {
-		System.out.println("You clicked me!");
-		label.setText("Hello World!");
+	private MenuButton inventoryButton;
+
+	@FXML
+	private MenuButton peakButton;
+
+	//Buttons
+	@FXML
+	private Button useButton;
+
+	@FXML
+	private Button hintButton;
+
+	@FXML
+	private Button dropButton;
+
+	@FXML
+	private Button takeButton;
+
+	@FXML
+	private Button inspectButton;
+
+	@FXML
+	private Button northButton;
+
+	@FXML
+	private Button westButton;
+
+	@FXML
+	private Button eastButton;
+
+	@FXML
+	private Button southButton;
+
+	//WebView
+	@FXML
+	private WebView console;
+
+	//Action events
+	@FXML
+	private void roomAction(ActionEvent event) {
+
 	}
-	
+
+	@FXML
+	private void inventoryAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	private void peakAction(ActionEvent event) {
+
+	}
+
+	//Buttons
+	@FXML
+	private void useAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	private void hintAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	private void dropAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	private void takeAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	private void inspectAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	private void northAction(ActionEvent event) {
+		String startMsg = businessMediator.start();
+		console.getEngine().loadContent(startMsg);
+	}
+
+	@FXML
+	private void westAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	private void eastAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	private void southAction(ActionEvent event) {
+
+	}
+
+	/**
+	 * Set the business mediator to be used by this controller.
+	 *
+	 * @param bm the business mediator to use
+	 */
+	public void setBusinessMediator(BusinessMediator bm) {
+		this.businessMediator = bm;
+	}
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		// TODO
-	}	
-	
+
+	}
+
 }
